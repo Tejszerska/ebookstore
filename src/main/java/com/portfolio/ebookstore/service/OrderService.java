dc4874f9a3ac8e9b67fcbf9dcb7aba619fdb341f
Basia <barbarawojciechowskaa@gmail.com>
2023-12-24 22:26:51 +0100

    the gist of it

--- src/main/java/com/portfolio/ebookstore/service/OrderService.java
+++ src/main/java/com/portfolio/ebookstore/service/OrderService.java
@@ -3,13 +3,13 @@ package com.portfolio.ebookstore.service;
 import com.portfolio.ebookstore.components.ShoppingCart;
 import com.portfolio.ebookstore.entities.Ebook;
 import com.portfolio.ebookstore.entities.Order;
-import com.portfolio.ebookstore.model.dto.OrderDto;
+import com.portfolio.ebookstore.model.dto.UserDto;
 
 import java.util.List;
 
 public interface OrderService {
     List<Order> getOrders();
     List<Ebook> getEbooksFromPastOrders(Long id);
-    void placeOrder();
+    void placeOrder(UserDto userDto);
     List<Ebook> getEbooksFromCart(ShoppingCart shoppingCart);
     Order getOrderById(Long orderId);    }
